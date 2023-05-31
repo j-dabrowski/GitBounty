9244b2887a23b4c491ed1b14f576cbe03a3a75c4
Alejandro <agmguindo@gmail.com>
2023-05-31 20:38:02 +0200

    added a example of protected route in the /issues page

--- frontend/pages/issues/index.tsx
+++ frontend/pages/issues/index.tsx
@@ -1,5 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useAccount } from 'wagmi'
+import { useRouter } from 'next/router';
 
-export const Issues = () => {
+export default function Issues () {
+    const account = useAccount()
+    const router = useRouter()
+  useEffect(()=>{
+    if(!account.isConnected){
+
+      router.push('/pepe');
+    }
+
+  },[account.isConnected, router])
   return <div>Issues</div>
 }
+
